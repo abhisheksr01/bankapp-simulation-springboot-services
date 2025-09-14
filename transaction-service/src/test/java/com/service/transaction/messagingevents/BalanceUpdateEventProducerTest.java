7a4c0a6586c9cb8605a660889fa9f061260647a7
abhisheksr01 <abhisheksingh2691@gmail.com>
2025-09-14 21:41:09 +0100

    refactor(transaction-service): refactor code and test for gradle jave dep bump

--- transaction-service/src/test/java/com/service/transaction/messagingevents/BalanceUpdateEventProducerTest.java
+++ transaction-service/src/test/java/com/service/transaction/messagingevents/BalanceUpdateEventProducerTest.java
@@ -1,27 +1,34 @@
 package com.service.transaction.messagingevents;
 
+import com.service.transaction.model.BalanceUpdateVO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.kafka.support.SendResult;
+
+import java.util.concurrent.CompletableFuture;
 
 import static com.service.transaction.helper.TestData.getBalanceUpdateEventMessage;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 class BalanceUpdateEventProducerTest {
     public static final String MOCK_TOPIC_NAME = "mock-topicName";
     private BalanceUpdateEventProducer balanceUpdateEventProducer;
-    private KafkaTemplate mockKafkaTemplate = mock(KafkaTemplate.class);
+    private KafkaTemplate<String, BalanceUpdateVO> mockKafkaTemplate;
 
     @BeforeEach
     void setUp() {
+        mockKafkaTemplate = mock(KafkaTemplate.class);
         balanceUpdateEventProducer = new BalanceUpdateEventProducer(mockKafkaTemplate, MOCK_TOPIC_NAME);
     }
 
     @Test
     void produceEventMessage_shouldCallTheKafkaTemplate_whenTransactionVOIsPassed() {
-        when(mockKafkaTemplate.send(MOCK_TOPIC_NAME, getBalanceUpdateEventMessage())).
-                thenReturn(mock(ListenableFuture.class));
+        CompletableFuture<SendResult<String, BalanceUpdateVO>> future = new CompletableFuture<>();
+        when(mockKafkaTemplate.send(eq(MOCK_TOPIC_NAME), any(BalanceUpdateVO.class)))
+                .thenReturn(future);
 
         balanceUpdateEventProducer.publishUpdateBalance(getBalanceUpdateEventMessage());
 
