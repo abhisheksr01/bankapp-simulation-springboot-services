054af24b1c12a564675c22d465a06f7c61cd0554
abhisheksr01 <abhisheksingh2691@gmail.com>
2025-09-14 01:27:26 +0100

    build(balance-service): bump gradle java version & refactor the code

--- balance-service/src/main/java/com/service/balance/BalanceApplication.java
+++ balance-service/src/main/java/com/service/balance/BalanceApplication.java
@@ -1,12 +1,11 @@
 package com.service.balance;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BalanceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BalanceApplication.class, args);
+//        SpringApplication.run(BalanceApplication.class, args);
     }
 }
