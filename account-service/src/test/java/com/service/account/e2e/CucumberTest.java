22da0e58f4f7df1b60b65576b18cb5c4b86a249e
abhisheksr01 <abhisheksingh2691@gmail.com>
2025-09-14 01:05:10 +0100

    test(account-service): fix e2e cucumber test

--- account-service/src/test/java/com/service/account/e2e/CucumberTest.java
+++ account-service/src/test/java/com/service/account/e2e/CucumberTest.java
@@ -1,13 +1,22 @@
 package com.service.account.e2e;
 
+import com.service.account.AccountApplication;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources",
-        plugin = {
-                "pretty", "html:build/reports/cucumber", "json:build/reports/cucumber/cucumber.json"
-        })
+@CucumberContextConfiguration
+@SpringBootTest(
+        classes = AccountApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+@CucumberOptions(features = "src/test/resources", plugin = {"pretty",
+        "html:build/reports/cucumber/cucumber-report.html"})
+@AutoConfigureEmbeddedDatabase
 public class CucumberTest {
 }
